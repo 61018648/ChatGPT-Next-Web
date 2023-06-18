fa8d4ccdb7efabc8773093bbf4e3ac74e818eae7
61018648 <82555345+61018648@users.noreply.github.com>
2023-06-19 00:07:33 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT Next Web Free By Star",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
